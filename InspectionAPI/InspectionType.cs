a76a8ad04e76c9a1ed80b013171891042c234d59
safwendr <safwendr@hotmail.com>
2022-03-27 17:59:35 +0100

    Add project files.

---
+++ InspectionAPI/InspectionType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionAPI
+{
+    public class InspectionType
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string InspectName { get; set; } = string.Empty;
+    }
+}
